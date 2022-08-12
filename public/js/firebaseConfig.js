89d752f122d942fb421e2f9fc2dfe3cca0ec0b95
Sergio Alvarez <ingeshare@gmail.com>
2022-08-11 21:12:08 -0500

    Adding Firebase Config Script

---
+++ public/js/firebaseConfig.js
@@ -0,0 +1,22 @@
+
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.2/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.9.2/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyDx30RoOx4sNb5m3nO_F8NaSAOYkaiRAkc",
+    authDomain: "capsula-7f5ab.firebaseapp.com",
+    projectId: "capsula-7f5ab",
+    storageBucket: "capsula-7f5ab.appspot.com",
+    messagingSenderId: "852385578487",
+    appId: "1:852385578487:web:04f411f72db3591b1882f2",
+    measurementId: "G-40EYEH26YY"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
--- public/js/saveTable.js
+++ public/js/saveTable.js
@@ -1,21 +1,41 @@
 let buttonSave = document.getElementById('button-save');
 
-function fnButtonSave(){
-    alert("alert in button save function");
-
-    //Add a new document in collection "cities"
-   //await setDoc(doc(db, "cities", "LA"), {
-     // name: "Los Angeles",
-     // state: "CA",
-    //  country: "USA"
-   // });
-
-}
 
 
-function fnSaveTableInit(){
-  buttonSave.addEventListener('click', fnButtonSave);
-}
+
+
 
 
 window.addEventListener('load', fnSaveTableInit);
+
+
+function fnButtonSave(){
+    alert("alert in button save function");
+
+        // Add a new document in collection "cities"
+    db.collection("cities").doc("LA").set({
+      name: "Los Angeles",
+      state: "CA",
+      country: "USA"
+    })
+    .then(() => {
+      console.log("Document successfully written!");
+    })
+    .catch((error) => {
+      console.error("Error writing document: ", error);
+    });
+
+}
+
+
+
+
+function fnSaveTableInit(){
+
+
+  buttonSave.addEventListener('click', fnButtonSave);
+
+
+}
+
+
